68f375f0a3b3142e5f2fa32564db42a2c881fd8c
kennethAsher <2573334038@qq.com>
2020-08-14 17:24:44 +0800

    启动清洗caseparty程序，并重命名

--- src/main/java/com/bangying/main/CasePartyHandler.java
+++ src/main/java/com/bangying/main/CasePartyHandler.java
@@ -20,8 +20,8 @@ import java.util.Map;
  * Author:         KennethAsher
  * Description:    此方法是清洗裁判文书，从之中找到原告，被告以及案由等。（包括诉求方身份）
  */
-public class CaseOartyHandler {
-    private final static Logger logger = LoggerFactory.getLogger(CaseOartyHandler.class);
+public class CasePartyHandler {
+    private final static Logger logger = LoggerFactory.getLogger(CasePartyHandler.class);
 
     public static void main(String[] args) throws IOException {
         logger.info("开始执行清洗裁判文书，寻找内容原告，被告，案由");
@@ -29,7 +29,7 @@ public class CaseOartyHandler {
         JavaRDD<String> text_rdd = spark_context.textFile(Constant.getProperty("organ_data_path"));
         logger.info("开始清洗。。。");
         JavaRDD<String> rdd_result = CaseParty.cleanCaseParty(text_rdd);
-        rdd_result.repartition(10);
+        rdd_result.repartition(100);
         rdd_result.saveAsTextFile(Constant.getProperty("case_party_path"));
     }
 }
